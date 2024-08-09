45b61dd476f30c285ef0626f1b08a7195d10252b
daotronghoan <daotronghoandz@gmail.com>
2024-08-09 16:32:00 +0700

    Add project files.

---
+++ WebApplication3.8/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3._8.Models;
+
+namespace WebApplication3._8.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        private Model1 db = new Model1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var user = db.Taikhoans.Where(u => u.tendn == username && u.matkhau == password).FirstOrDefault();
+            if (user == null)
+            {
+                ViewBag.errLogin = "Sai tên đăng nhập hoặc mật khẩu";
+                return View("Login");
+            }
+            else
+            {
+                Session["username"] = username;
+                return RedirectToAction("Index", "NhanViens"); //chạy tới action Index
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session["username"] = null;
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
---
+++ WebApplication3.8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication3._8.MvcApplication" Language="C#" %>
